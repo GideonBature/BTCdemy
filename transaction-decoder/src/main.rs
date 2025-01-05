8249f0aa7633c7beb5162cd4af7aee6b35f6e7be
GideonBature <infoaboutgideon@gmail.com>
2025-01-05 22:15:31 +0100

    Feat: reading compact size function

--- transaction-decoder/src/main.rs
+++ transaction-decoder/src/main.rs
@@ -1,6 +1,27 @@
 use hex;
 use std::io::Read;
 
+fn read_compact_size(transaction_bytes: &mut &[u8]) -> u64 {
+    let mut compact_size = [0_u8; 1];
+    transaction_bytes.read(&mut compact_size).unwrap();
+
+    if (0..253).contains(&compact_size[0]) {
+        compact_size[0] as u64
+    } else if compact_size[0] == 253 {
+        let mut buffer = [0; 2];
+        transaction_bytes.read(&mut buffer).unwrap();
+        u16::from_le_bytes(buffer) as u64
+    } else if compact_size[0] == 254 {
+        let mut buffer = [0; 4];
+        transaction_bytes.read(&mut buffer).unwrap();
+        u32::from_le_bytes(buffer) as u64
+    } else {
+        let mut buffer = [0; 8];
+        transaction_bytes.read(&mut buffer).unwrap();
+        u64::from_le_bytes(buffer)
+    }
+}
+
 #[allow(unused)]
 fn read_version(transaction_bytes: &mut &[u8]) -> /* Result<(), FromHexError> */ u32 {
     let mut buffer = [0; 4];
