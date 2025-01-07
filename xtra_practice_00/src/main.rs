4a1cd55f353718cc1f343771034df7250bca1427
GideonBature <infoaboutgideon@gmail.com>
2025-01-07 17:01:12 +0100

    Feat: solved

--- xtra_practice_00/src/main.rs
+++ xtra_practice_00/src/main.rs
@@ -1,4 +1,18 @@
+use std::fmt::{self, Display, Formatter};
+
+struct CustomVec(Vec<u8>);
+
+impl Display for CustomVec {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "Values:\n")?;
+        for v in &self.0 {
+            write!(f, "\t{}", v)?;
+        }
+        Ok(())
+    }
+}
+
 fn main() {
-    let vec: Vec::<u8> = vec![0, 0, 0, 0, 0];
-    println!("Vec: {:?}", vec);
+    let vec = CustomVec(vec![0, 0, 0, 0, 0]);
+    println!("Vec: {}", vec);
 }
